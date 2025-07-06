48972f9b68d3b13c6c03ad30b5a4794104dd3d59
Hansz12 <farhanamalik199@gmail.com>
2025-07-06 11:40:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-// server.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from Render!');
 });
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello, API is working!' });
-});
-
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
